c4957cbe7300dae9d0f7d3084dbd732d7c6311d4
ramazangenis02 <ramazangenis02@gmail.com>
2021-12-01 19:31:28 +0300

    updated

--- src/navbar.js
+++ src/navbar.js
@@ -12,7 +12,21 @@ const navBar = (() => {
   navBarContainer.appendChild(navBarWrapper);
   mainContent.appendChild(navBarContainer);
 
-  
+  const home = document.createElement("span");
+  const menu = document.createElement("menu");
+  const contact = document.createElement("contact");
+
+  home.classList.add("nav-links");
+  menu.classList.add("nav-links");
+  contact.classList.add("nav-links");
+
+  home.innerText = "Home";
+  menu.innerText = "Menu";
+  contact.innerText = "Contact";
+
+  navBarWrapper.appendChild(home);
+  navBarWrapper.appendChild(menu);
+  navBarWrapper.appendChild(contact);
 })();
 
 export default navBar;
